71885498faf92862f286af66d2533dfa70a7f1cc
Shrey03 <pshrey619@gmail.com>
2022-01-02 20:48:14 -0500

    First Commit , learning REACT

---
+++ first-react/src/App.js
@@ -0,0 +1,16 @@
+
+import Todo from "./components/todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My TODOS</h1>
+      <Todo text="Learn React" />
+      <Todo text="Crack Interview" />
+      <Todo text="Get Job" />
+     
+    </div>
+  );
+}
+
+export default App;
