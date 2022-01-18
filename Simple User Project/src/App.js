c7865c381af17eaf1c57e7cccfe15e64a9d8bc41
Shrey03 <pshrey619@gmail.com>
2022-01-17 20:35:59 -0500

    first commit

--- Simple User Project/src/App.js
+++ Simple User Project/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-
+import React from "react";
 
 function App() {
   return (
     <div>
-
+      <h2>Starting project</h2>
+      <p>Creating user by taking input</p>
     </div>
   );
 }
