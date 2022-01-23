510444ff30e32cf2e847b616a190fd402704cf50
Shrey03 <pshrey619@gmail.com>
2022-01-22 23:55:38 -0500

    Added CSS  components

--- Simple User Project/src/components/Users/AddUser.js
+++ Simple User Project/src/components/Users/AddUser.js
@@ -1,16 +1,20 @@
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">UserName</label>
-      <input id="username" type="text"></input>
-      <label htmlFor="age">Age</label>
-      <input id="age" type="number"></input>
-      <button type="submit"> ADD USER</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">UserName</label>
+        <input id="username" type="text"></input>
+        <label htmlFor="age">Age</label>
+        <input id="age" type="number"></input>
+        <button type="submit"> ADD USER</button>
+      </form>
+    </Card>
   );
 };
 
